974ab65f2790bc8cc73880eb4d0f89cba23239d4
Dongseon Lee <eastline@maycan.co.kr>
2020-09-19 14:45:17 +0900

    Updates

---
+++ precache-manifest.fc9bca8f6508553cb6839bb9e38b7e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01c19d85dae6105f4b2618a6587cc87",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9b11c42c38c3e2ffc19b",
+    "url": "/movie_app/static/css/main.888d1aad.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b11c42c38c3e2ffc19b",
+    "url": "/movie_app/static/js/main.46559cea.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
